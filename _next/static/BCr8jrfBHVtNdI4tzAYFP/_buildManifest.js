d538722f3605e44dadb75c7c256e6e61ee7e0985
sanguneo <sanguneo@acon3d.com>
2023-09-12 10:40:07 +0900

    Updates

--- _next/static/BCr8jrfBHVtNdI4tzAYFP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/134-d210d399e74f172e.js","static/chunks/pages/index-36cc3496bad400fa.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
