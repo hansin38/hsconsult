a88e2ad2c78b75584b4cc2adf4688aaf19125ad1
sanguneo <sanguneo@acon3d.com>
2023-09-12 10:51:34 +0900

    - 패키지 업데이트

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 const nextConfig = {
   // reactStrictMode: false,
   trailingSlash: true,
-  webpack(config) {
-    config.resolve.alias['@'] = '/src';
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgo: true,
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'preset-default',
-                  params: {
-                    overrides: {
-                      removeViewBox: false,
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    return config;
-  },
   compiler: {
     emotion: true,
   }
