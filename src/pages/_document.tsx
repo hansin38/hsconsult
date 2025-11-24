0be2966a315c7e80a4536fd98173fccb9b729095
hsconsult <hsconsult>
2025-11-24 19:51:44 +0900

    chore: 기본설정 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable @next/next/no-title-in-document-head */
 import { Head, Html, Main, NextScript } from 'next/document';
-import React from 'react';
 
 export default function MyDocument() {
-  return (
-    <Html>
-      <Head>
-        <meta name="description" content="한신콘설탄트" />
-        <link rel="icon" href="/images/favicon.png" type="image/x-icon" />
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="keywords" content="한신콘설탄트" />
-        <meta name="author" content="sanguneo, juno" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html>
+            <Head>
+                <meta name="description" content="한신콘설탄트" />
+                <link rel="icon" href="/images/favicon.png" type="image/x-icon" />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+                />
+                <meta name="theme-color" content="#000000" />
+                <meta name="keywords" content="한신콘설탄트" />
+                <meta name="author" content="sanguneo, juno" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
--- src/pages/api/inquiry/write.ts
+++ src/pages/api/inquiry/write.ts
@@ -2,18 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { sql } from '@vercel/postgres';
 
 type ResponseData = {
-  status: 'success' | 'error';
-  data: any;
-}
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  if (req.method.toUpperCase() !== 'PUT') return res.status(400).end();
-  const { content, name, email, phone } = req.body;
-  const data = await sql`
+    status: 'success' | 'error';
+    data: any;
+};
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+    if (req.method.toUpperCase() !== 'PUT') return res.status(400).end();
+    const { content, name, email, phone } = req.body;
+    const data = await sql`
 INSERT INTO inquiry (timestamp, content, name, email, phone)
 VALUES (${Math.floor(new Date().getTime() / 1000)}, ${content}, ${name}, ${email}, ${phone});
 `;
-  return res.status(200).json({ status: 'success', data });
+    return res.status(200).json({ status: 'success', data });
 }
