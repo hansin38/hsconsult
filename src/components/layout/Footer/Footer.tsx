901de4544e8cc70a86ffc2561a1eadafe6b15dd2
hansin38 <149050267+hansin38@users.noreply.github.com>
2025-11-24 13:27:05 +0900

    Refactor Footer component styles import and container

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { Logo } from '@/components/svg/Logo';
-import { Contacts, Copyright, Container } from './.styles';
+import { Contacts, Copyright, FooterContainer } from './Footer.styles';
 
 
 const Footer = (): React.ReactElement => {
   const router = useRouter();
   return (
-    <Container>
+    <FooterContainer>
       <Logo onClick={()=>router.push('/')} bleach height={32}/>
       <Contacts>
         <div><span>(주)한신콘설탄트</span><span>대표자 : 한상로, 이광휘</span></div>
@@ -15,7 +15,7 @@ const Footer = (): React.ReactElement => {
         <div><span>서울지사 : 서울특별시 송파구 가락로240 (방이동)</span><span> 서울지사 : (TEL) 02-417-6761</span><span>(FAX) 02-417-3302</span></div>
       </Contacts>
       <Copyright>COPYRIGHT(C) 주식회사한신콘설탄트. all right reserved.</Copyright>
-    </Container>
+    </FooterContainer>
   );
 };
 
