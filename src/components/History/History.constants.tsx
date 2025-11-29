fc27b2675564931648319ec2db06b4b9203d004e
jeong3184 <jeong3184@nate.com>
2025-11-29 11:47:23 +0900

    연혁 순서 및 내용 변경

--- src/components/History/History.constants.tsx
+++ src/components/History/History.constants.tsx
@@ -1,54 +1,67 @@
 export const INITIAL_HISTORY_DATA = [
-  {
-    yearbundle: '설립일 - 1999',
-    years: [
-      { year: 1999, content: [
-          { description: 'ISO 9001 인증 취득(전기설계, 감리, 공사)' },
-          { description: '전기공사업 등록(부산-00665)' },
+    {
+        yearbundle: '2000 - 현재',
+        years: [
+            { year: 2003, content: [{ description: '자본금 305,000천원으로 증자' }] },
+            {
+                year: 2023,
+                content: [
+                    { description: '철도시설 안전진단 전문기관등록(정보통신) 부산광역시 제 2023-6호' },
+                    { description: '엔지니어링사업자분야 추가신고 (신고분야:철도신호)' },
+                ],
+            },
+            { year: 2025, content: [{ description: '한국산업기술진흥협회 기업부설연구소 인정 획득' }] },
         ],
-      },
-      { year: 1998, content: [
-          { description: 'ISO9001 인증 취득(전기공사의 설계 및 감리)' },
+    },
+    {
+        yearbundle: '설립일 - 1999',
+        years: [
+            {
+                year: 1999,
+                content: [
+                    { description: 'ISO 9001 인증 취득(전기설계, 감리, 공사)' },
+                    { description: '전기공사업 등록(부산-00665)' },
+                ],
+            },
+            { year: 1998, content: [{ description: 'ISO9001 인증 취득(전기공사의 설계 및 감리)' }] },
+            {
+                year: 1997,
+                content: [
+                    { description: '전력시설물 종합 설계업 등록(제DA1호)' },
+                    { description: '전력시설물 종합 감리업 등록(제SA3호)' },
+                    { description: '전력시설물 설계감리자 확인(제SD2호)' },
+                ],
+            },
+            {
+                year: 1996,
+                content: [
+                    { description: '"액세스플로어" 실용신안 등록(101354호)-대한민국특허청' },
+                    { description: '"PIT COVER" 실용신안 등록(094811호)-대한민국특허청' },
+                    { description: '소방설비 설계, 감리 업 등록' },
+                ],
+            },
+            {
+                year: 1993,
+                content: [
+                    { description: '서울지사 설립' },
+                    { description: '군납업체 보안측정 유자격업체 등록' },
+                    { description: '엔지니어링 활동주체 신고(한국엔지니어링협회)-신고번호 05-008호' },
+                ],
+            },
+            {
+                year: 1983,
+                content: [
+                    { description: '법인설립 전환-"(주) 한신콘설탄트"' },
+                    { description: '부산지방 국토관리청 토지측량업 등록' },
+                ],
+            },
+            {
+                year: 1979,
+                content: [
+                    { description: '한신콘설탄트 설립' },
+                    { description: '전문 기술용역업 등록 (전기부분, 발송배전, 전기기기) 과학기술처 제3-47호' },
+                ],
+            },
         ],
-      },
-      { year: 1997, content: [
-          { description: '전력시설물 종합 설계업 등록(제DA1호)' },
-          { description: '전력시설물 종합 감리업 등록(제SA3호)' },
-          { description: '전력시설물 설계감리자 확인(제SD2호)' },
-        ],
-      },
-      { year: 1996, content: [
-          { description: '"액세스플로어" 실용신안 등록(101354호)-대한민국특허청' },
-          { description: '"PIT COVER" 실용신안 등록(094811호)-대한민국특허청' },
-          { description: '소방설비 설계, 감리 업 등록' },
-        ],
-      },
-      { year: 1993, content: [
-          { description: '서울지사 설립' },
-          { description: '군납업체 보안측정 유자격업체 등록' },
-          { description: '엔지니어링 활동주체 신고(한국엔지니어링협회)-신고번호 05-008호' },
-        ],
-      },
-      { year: 1983, content: [
-          { description: '법인설립 전환-"(주) 한신콘설탄트"' },
-          { description: '부산지방 국토관리청 토지측량업 등록' },
-        ],
-      },
-      { year: 1979, content: [
-          { description: '한신콘설탄트 설립' },
-          { description: '전문 기술용역업 등록 (전기부분, 발송배전, 전기기기) 과학기술처 제3-47호' },
-        ],
-      },
-    ],
-  },
-  {
-    yearbundle: '2000 - 현재',
-    years: [
-      {
-        year: 2023,
-        content: [{ description: '철도시설 안전진단 전문기관등록(정보통신) 부산광역시 제 2023-6호' }, { description: '엔지니어링사업자분야 추가신고 (신고분야:철도신호)' }],
-      },
-      { year: 2003, content: [{ description: '자본금 305,000천원으로 증자' }] },
-    ],
-  },
+    },
 ];
